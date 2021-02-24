c9ca5abc3bd6349b6c4cfcf1c20a8be304f1a375
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-02-24 12:39:39 -0300

    Finalizado testes de usuários

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+
 import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
@@ -8,14 +9,10 @@ class UserController {
 
         const usersRepository = getCustomRepository(UsersRepository);
 
-        const userAlreadyExists = await usersRepository.findOne({
-            email
-        });
+        const userAlreadyExists = await usersRepository.findOne({ email });
 
         if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists",
-            });
+            return response.status(400).json({ error: 'User already exists!' });
         }
 
         const user = usersRepository.create({
@@ -25,7 +22,7 @@ class UserController {
 
         await usersRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
