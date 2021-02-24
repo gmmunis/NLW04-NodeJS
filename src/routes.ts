21578b54dce57f4daa73cc88f3debd0f1727986c
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-02-24 10:19:39 -0300

    método show para listar todas as pesquisas

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,8 @@ const surveyController = new SurveyController
 
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
+router.get("/surveys", surveyController.show);
+
 
 
 export { router };
