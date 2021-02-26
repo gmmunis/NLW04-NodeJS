72873d52432a59ea6f00b83a6e020b087d4c1a1e
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-02-26 17:24:25 -0300

    Controller de resposta do usuário, validando usuário existente e alteração da nota de resposta do e-mail

--- src/repositories/SurveysUsersRepository.ts
+++ src/repositories/SurveysUsersRepository.ts
@@ -2,6 +2,10 @@ import { EntityRepository, Repository } from "typeorm";
 import { SurveyUser } from "../models/SurveyUser";
 
 @EntityRepository(SurveyUser)
-class SurveysUsersRepository extends Repository<SurveyUser> {}
+class SurveysUsersRepository extends Repository<SurveyUser> {
+    static findOne(arg0: {}) {
+        throw new Error('Method not implemented.');
+    }
+}
 
 export { SurveysUsersRepository }
