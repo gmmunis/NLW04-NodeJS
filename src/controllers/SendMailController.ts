81146ad9cb9cb6de11ed9e75f4233d66b9104bd7
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-02-26 11:45:27 -0300

    Envio de email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import { SurveyRepository } from '../repositories/SurveyRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import { UsersRepository } from '../repositories/UsersRepository';
+import SendMailService from '../services/SendMailService';
 
 class SendMailController {
     async execute(request: Request, response: Response) {
@@ -20,9 +21,9 @@ class SendMailController {
             });
         }
 
-        const surveyAlreadyExists = await surveyRepository.findOne({id: survey_id});
+        const survey = await surveyRepository.findOne({id: survey_id});
 
-        if(!surveyAlreadyExists) {
+        if(!survey) {
             return response.status(400).json({
                 error: "Survey does not exists!"
             })
@@ -33,6 +34,8 @@ class SendMailController {
             survey_id,
         });
         await surveysUsersRepository.save(surveyUser);
+
+        await SendMailService.execute(email, survey.title, survey.description);
 
         return response.json(surveyUser);
     }
